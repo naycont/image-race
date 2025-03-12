8767117f235f376812542e1043f00c9f201417af
Nayeli Contreras <naycont@outlook.com>
2025-03-12 16:57:00 -0600

    home view styled

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,40 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="d-flex justify-center flex-column align-center">
-    <span class="text-h4 text-center mb-4">
-      Ayuda a los vendedores de
-      <span class="text-primary">Alegra</span>
-      a ganar una carrera.
-    </span>
+  <div>
+    <div class="d-flex justify-center align-start">
+      <span class="text-h4">
+        Ayuda a los vendedores de
+        <span class="text-primary">Alegra</span>
+        a ganar una carrera.
+      </span>
+    </div>
 
-    <div class="my-4">
-      <p class="d-flex flex-column">
+    <div class="d-flex justify-center align-center mt-8">
+      <div class="d-flex flex-column">
         <span>
-          <v-avatar color="primary" variant="outlined">
+          <v-avatar color="primary" variant="outlined" class="mr-2">
             <span class="text-h5">1</span>
           </v-avatar>
           Usa el buscador para encontrar imagenes que cada vendedor te presentará
         </span>
-
         <span class="mt-3">
-          <v-avatar color="primary" variant="outlined">
+          <v-avatar color="primary" variant="outlined" class="mr-2">
             <span class="text-h5">2</span>
           </v-avatar>
           Elige la imagen que mas te guste y el vendedor obtendra 3 puntos
         </span>
 
         <span class="mt-3">
-          <v-avatar color="primary" variant="outlined">
+          <v-avatar color="primary" variant="outlined" class="mr-2">
             <span class="text-h5">3</span>
           </v-avatar>
-          Continua buscando imágenes y el ganador será el que alcance 20 puntos.
+          Continua buscando imágenes y el ganador será el que alcance 20 puntos
         </span>
-      </p>
+      </div>
     </div>
 
-    <span class="my-4 text-secondary text-h6">Que comience la carrera!</span>
-    <v-btn color="primary">Empezar</v-btn>
+    <div class="d-flex justify-center align-center flex-column mt-4">
+      <span class="my-4 text-accent text-h5">Que comience la carrera!</span>
+      <v-btn color="primary" flat size="large">Empezar</v-btn>
+    </div>
   </div>
 </template>
