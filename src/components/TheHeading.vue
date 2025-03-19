7bb2b4859c79987bf525c835e23a94ccaab36679
Nayeli Contreras <naycont@outlook.com>
2025-03-18 19:02:05 -0600

    implementing warning on attemping to exit page

--- src/components/TheHeading.vue
+++ src/components/TheHeading.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
+import { useDialogStore } from '@/stores/dialog'
+import { DIALOG_TYPES } from '@/utils/constants'
+import { useScoreStore } from '@/stores/score'
 
 const router = useRouter()
 
-const goToHome = () => {
-  router.push({ name: 'home' })
+const dialogStore = useDialogStore()
+const scoreStore = useScoreStore()
+
+const dialogConfiguration = computed(() => dialogStore.dialog)
+const currenRoute = computed(() => router.currentRoute.value.name)
+
+const confirmExit = () => {
+  const dialog = dialogStore.dialog
+
+  dialogStore.activeDialog({
+    ...dialog,
+    title: 'Confirmación',
+    message: '¿Estás seguro de que deseas abandonar?, al confirmar perderás tu progreso',
+    type: DIALOG_TYPES.confirm
+  })
 }
 
-const currenRoute = computed(() => router.currentRoute.value.name)
+watch(dialogConfiguration, (nextDialogConfiguration) => {
+  if (nextDialogConfiguration.confirmed) {
+    scoreStore.initScore()
+    router.push({ name: 'home' })
+  }
+})
 </script>
 
 <template>
@@ -19,7 +40,7 @@ const currenRoute = computed(() => router.currentRoute.value.name)
       imágenes del mundo
     </v-app-bar-title>
     <template v-slot:append v-if="currenRoute !== 'home'">
-      <v-btn icon="logout" color="grey-darken-1" @click="goToHome"></v-btn>
+      <v-btn icon="logout" color="grey-darken-1" @click="confirmExit"></v-btn>
     </template>
   </v-app-bar>
 </template>
