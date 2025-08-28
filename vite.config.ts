d7ff780ae94b1a71c6e563b119bdb62e586a9ac3
Nayeli Contreras <naycont@gmail.com>
2025-08-28 13:16:39 -0600

    upadte base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/image-race/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
