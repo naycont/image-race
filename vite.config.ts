857dc7253d4c25116e4c8ad1dd2016eef78a12a8
Nayeli Contreras <naycont@gmail.com>
2025-08-28 13:21:30 -0600

    fix base url path

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/',
+  base: '/image-race/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
